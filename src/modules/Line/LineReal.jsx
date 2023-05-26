43f3305d084bae9dc5d04ea7a654b789a69fbc8d
A-rya-n <aryan31.purohit@gmail.com>
2023-05-26 15:22:57 +0530

    working on finnnub

---
+++ src/modules/Line/LineReal.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+// import { useFinnhub } from "react-finnhub";
+
+// const LineReal = () => {
+//   const [state, setState] = useState();
+//   const finnhub = useFinnhub();
+
+//   finnhub.quote("AAPL", (data) => {
+//     setState(data);
+//   });
+
+//   return <div>{console.log(state)}</div>;
+// };
+
+// export default LineReal;
